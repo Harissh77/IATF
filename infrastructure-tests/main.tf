32b74f761eded897dd27d9b2f4aa7d1aac22abba
Harish Gautham <harissh.gautam@gmail.com>
2024-10-26 07:44:10 +0530

    Update main.tf

--- infrastructure-tests/main.tf
+++ infrastructure-tests/main.tf
@@ -18,27 +18,6 @@ variable "instance_name" {
 type = string
 }
 
-
-variable "services" {
-  type = map(object({
-    node = string
-    kind = string
-  }))
-  description = "List of services and their metadata"
-}
- 
-variable "service_kind" {
-  type        = string
-  description = "Service kind to search"
-}
- 
-locals {
-  ip_addresses = toset([
-    for service, service_data in var.services :
-    replace(replace(split(".", service_data.node)[0], "ip-", ""), "-", ".") if service_data.kind == var.service_kind
-  ])
-}
-
 resource "aws_instance" "create_instance" {
   ami           = "ami-04e914639d0cca79a"
   instance_type = "t2.micro"
@@ -50,7 +29,3 @@ resource "aws_instance" "create_instance" {
 }
 
  
-resource "local_file" "ip_addresses" {
-  content  = jsonencode(local.ip_addresses)
-  filename = "./${var.service_kind}.hcl"
-}
