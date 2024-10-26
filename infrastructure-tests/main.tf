29f63e3f20c64ae84e070e136cbcb24754e1e077
Harissh77 <harissh.gautam@gmail.com>
2024-10-26 13:41:41 +0530

    Adding AWS config & credentials files and default location

--- infrastructure-tests/main.tf
+++ infrastructure-tests/main.tf
@@ -14,30 +14,22 @@ provider "aws" {
   profile = "iatf"
 }
 
+variable "AWS_CONFIG_FILE" { 
+type= string 
+default = "./config/config"
+} 
+
+variable "AWS_SHARED_CREDENTIALS_FILE" { 
+type= string 
+default = "./config/credentials"
+} 
+
 variable "instance_name" {
 type = string
+default = "pipeline"
 }
 
 
-variable "services" {
-  type = map(object({
-    node = string
-    kind = string
-  }))
-  description = "List of services and their metadata"
-}
- 
-variable "service_kind" {
-  type        = string
-  description = "Service kind to search"
-}
- 
-locals {
-  ip_addresses = toset([
-    for service, service_data in var.services :
-    replace(replace(split(".", service_data.node)[0], "ip-", ""), "-", ".") if service_data.kind == var.service_kind
-  ])
-}
 
 resource "aws_instance" "create_instance" {
   ami           = "ami-04e914639d0cca79a"
@@ -50,7 +42,3 @@ resource "aws_instance" "create_instance" {
 }
 
  
-resource "local_file" "ip_addresses" {
-  content  = jsonencode(local.ip_addresses)
-  filename = "./${var.service_kind}.hcl"
-}
