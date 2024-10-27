644c454db9527392bea99d66a07809f6db8b5033
Harissh77 <harissh.gautam@gmail.com>
2024-10-27 23:30:04 +0530

    Adding Integration Test

---
+++ infrastructure-tests/Install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo yum install python -y
+sudo yum install pip -y  
+sudo python -m pip install pytest
---
+++ infrastructure-tests/integration-test/test_endpointchecks.py
@@ -0,0 +1,15 @@
+def test_TF1():
+    
+    assert 1 + 1 == 2
+    print('Running Test1')
+
+def test_TF2():
+    
+    assert 1 + 1 == 2
+    print('Running Test1')
+
+
+def test_TF3():
+    
+    assert 1 + 1 == 2
+    print('Running Test1')
