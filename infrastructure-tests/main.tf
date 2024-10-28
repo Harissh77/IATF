64df5cb4a8364ee1a4efb868338c0368ce393129
Harish Gautham <harissh.gautam@gmail.com>
2024-10-28 07:13:04 +0530

    Fixing the main.tf

--- infrastructure-tests/main.tf
+++ infrastructure-tests/main.tf
@@ -9,8 +9,6 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-
-
 variable "AWS_CONFIG_FILE" { 
 type= string 
 default = "./config/config"
@@ -26,25 +24,16 @@ type = string
 default = "pipeline"
 }
 
-<<<<<<< HEAD
 resource "aws_instance" "server" {
   ami = var.ami_id
   instance_type = var.instance_type
   vpc_security_group_ids = [ aws_security_group.instance.id ]
   availability_zone = var.availability_zone
-=======
-
-resource "aws_instance" "create_instance" {
-  ami           = "ami-04a37924ffe27da53"
-  instance_type = "t2.micro"
-
->>>>>>> e79f9c3bd7d8518c7e2665f1019d9935c5874d44
   tags = {
-    Name = "EC2-Server"
+    Name = "HG-EC2-Server"
   }
   user_data = <<-EOF
                 #!/bin/bash
-<<<<<<< HEAD
 	        sudo yum install python -y 
                 sudo yum install pip -y 
                 sudo python -m pip install pytest -y
@@ -55,15 +44,6 @@ resource "aws_instance" "create_instance" {
                 echo "Welcome to the IATF Demo !" > /var/www/html/index.html
                 EOF
    user_data_replace_on_change = true 
-=======
-                sudo yum install python -y
-                sudo yum install pip -y
-                sudo python -m pip install pytest -y 
-                echo "Terraform is easy!!!" > /var/www/html/index.html
-                EOF
-   user_data_replace_on_change = true 
-
->>>>>>> e79f9c3bd7d8518c7e2665f1019d9935c5874d44
 }
 
 
