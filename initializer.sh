676bb9265f88ba88dc0f3d0e24c27aa2e898bb98
Harissh77 <harissh.gautam@gmail.com>
2024-10-31 08:10:56 +0530

    A

--- initializer.sh
+++ initializer.sh
@@ -28,7 +28,7 @@ read -p "IATF> " maincmd
 echo ${maincmd}
 sleep 1
 
-if [[ ${maincmd,,} =~ "red hat"]] && [[  ${maincmd,,} =~ "build and test" ]] && [[ ${maincmd,,} =~ "with ai on"]] && [[ ${maincmd,,} =~ "ap-south-1" ]]; then
+if [[ "${maincmd,,}" =~ "red hat"]] && [[  "${maincmd,,}" =~ "build and test" ]] && [[ "${maincmd,,}" =~ "with ai on"]] && [[ "${maincmd,,}" =~ "ap-south-1" ]]; then
      curl -v -X POST http://43.205.229.97:8080/job/IATF_Pipeline/build --user Castor:114e9ebf0dfaa8b197b045da220ee7ad86
 else
      echo "Regrettably, I cannot process this command, either the regoin or the instance type is not suppoprted yet by IATF at the moment"
