1c9a073d88c1ec21e95121e705271d6ad5f11c90
Harish Gautham <harissh.gautam@gmail.com>
2024-11-02 20:37:18 +0530

    Update output.tf

--- infrastructure-tests/output.tf
+++ infrastructure-tests/output.tf
@@ -2,3 +2,8 @@ output "public_ip" {
     description = "public IP address of the web server"
     value = aws_instance.server.public_ip
 }
+
+output "instance_name" {
+    description = "Name of the Instance"
+    value = aws_instance.server.public_ip
+}
