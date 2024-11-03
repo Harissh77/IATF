9b0150304799b61983a7e577d09a7113affcf67d
Harish Gautham <harissh.gautam@gmail.com>
2024-11-03 20:58:18 +0530

    Adding new tests

--- Application/src/test/java/HGTest/HGTest/AppTest.java
+++ Application/src/test/java/HGTest/HGTest/AppTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(OutputCaptureExtension.class)
@@ -13,7 +14,43 @@ public class AppTest {
     @Test
     void checkInstace(CapturedOutput capture) throws Exception {
         App.main(new String[]{});
-        assertThat(capture.toString()).isEqualToIgnoringNewLines("Red Hat Linux");
+        assertThat(capture.toString()).contains("IATF-Instance");
+    }
+    
+    @Test
+    void publicIPCheck(CapturedOutput capture) throws Exception {
+        App.main(new String[]{});
+        assertThat(capture.toString()).isNotEmpty();
+    }
+    
+    @Test
+    void availabilityZoneTest(CapturedOutput capture) throws Exception {
+        App.availabilityZoneCheck(new String[]{});
+        assertThat(capture.toString()).contains("ap-south-1a");
+    }
+    
+    @Test
+    void instanceTypeTest(CapturedOutput capture) throws Exception {
+        App.instanceTypeCheck(new String[]{});
+        assertThat(capture.toString()).contains("t2.micro");
+    }
+    
+    @Test
+    void instanceStateTest(CapturedOutput capture) throws Exception {
+        App.instanceStateCheck(new String[]{});
+        assertThat(capture.toString()).contains("running");
+    }
+    
+    @Test
+    void subnetIDTest(CapturedOutput capture) throws Exception {
+        App.subnetIDCheck(new String[]{});
+        assertThat(capture.toString()).contains("subnet-");
+    }
+    
+    @Test
+    void tenancyTest(CapturedOutput capture) throws Exception {
+        App.tenancyCheck(new String[]{});
+        assertThat(capture.toString()).contains("default");
     }
     
     @Test
