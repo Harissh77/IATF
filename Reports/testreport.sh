d2aa9257974bf5140b4807733b3ba6a3e14b8273
Harissh77 <harissh.gautam@gmail.com>
2024-11-14 17:15:17 +0530

    Adding testreport script

---
+++ Reports/testreport.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export TESTMO_TOKEN="testmo_api_eyJpdiI6IjJJbTVBL0lhSnpTN3ozWkpaWG4rdkE9PSIsInZhbHVlIjoiSEN1VGVlc2dsaTVkWlJZd1NrOGhRNkZON2RhMlREZWVyNlQ4WUpBNjRmZz0iLCJtYWMiOiI3ZWU4NDgxOTRhNTEzNGE4MjNjOTRkZTdmNTcyNDIzNmY4MWZjMDQ2NDAxNTM4YjJiYTg0YTdmYTU2YzRlMjNmIiwidGFnIjoiIn0="
+echo $TESTMO_TOKEN
+testmo automation:run:submit --instance https://harishgautham.testmo.net --project-id 1 --name "Test Report" --source "IntegrationTest" --results **/target/surefire-reports/TEST-*.xml'
